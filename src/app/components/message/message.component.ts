642b71bdc273f7318d5264773fc806ac60c3088f
Adeilson Silva <adeilson.silva@agenciagenia.com.br>
2022-12-10 17:04:57 -0300

    ok

---
+++ src/app/components/message/message.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-message',
+  templateUrl: './message.component.html',
+  styleUrls: ['./message.component.css']
+})
+export class MessageComponent implements OnInit {
+
+  @Input()
+
+  message = "Mensagem de erro";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
